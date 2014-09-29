54f3a52dbe4c9085472909c867454ee23d255250
Antonio Tapiador del Dujo <atapiador@ucm.es>
2014-09-29 16:29:39 +0200

    Add mobileangular dependency

--- app/app.js
+++ app/app.js
@@ -3,7 +3,8 @@
 // Declare app level module which depends on views, and components
 angular.module('Pear2Pear', [
   'ngRoute',
-  'Pear2Pear.version'
+  'Pear2Pear.version',
+  'mobile-angular-ui'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/'});
