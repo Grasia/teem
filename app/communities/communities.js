e2f84015eaade714f2479ff528f4ac02e9a71826
Antonio Tapiador del Dujo <atapiador@ucm.es>
2014-10-01 15:51:35 +0200

    Community edit

--- app/communities/communities.js
+++ app/communities/communities.js
@@ -1,7 +1,18 @@
 'use strict';
 
 angular.module('Pear2Pear.communities', [])
-.controller('CommunitiesCtrl', ['$scope', '$location', function($scope, $location){
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+    when('/communities', {
+      templateUrl: 'communities/index.html',
+      controller: 'CommunitiesCtrl'
+    }).
+    when('/communities/:id/edit', {
+      templateUrl: 'communities/edit.html',
+      controller:'CommunitiesCtrl'
+    });
+}])
+.controller('CommunitiesCtrl', ['$scope', '$location', '$routeParams', function($scope, $location, $routeParams){
   $scope.communities = [
     {
       id: 1,
@@ -16,4 +27,26 @@ angular.module('Pear2Pear.communities', [])
       name: "Tabacalera"
     }
   ];
+
+  $scope.community = {
+    name: "P2Pvalue",
+    participants: [
+      {
+        name: "Marco"
+      },
+      {
+        name: "Primavera"
+      },
+      {
+        name: "Mayo"
+      },
+      {
+        name: "Samer"
+      }
+    ]
+  };
+
+  $scope.edit = function(id) {
+    $location.path('/communities/' + id + '/edit');
+  };
 }]);
