21eb829f250648118be203d2f42d65840f75040d
Antonio Tenorio Fornés <antoniotenorio@ucm.es>
2014-10-07 14:30:24 +0200

    cordova calendar plugin functioning

---
+++ app/setUpAndroidCordova.sh
@@ -0,0 +1,6 @@
+rm cordova.js
+rm cordova_plugins.js
+rm -Rf plugins
+ln -s ../mobile/platforms/android/assets/www/cordova.js
+ln -s ../mobile/platforms/android/assets/www/cordova_plugins.js
+ln -s ../mobile/platforms/android/assets/www/plugins
--- app/tasks/tasks.js
+++ app/tasks/tasks.js
@@ -67,25 +67,25 @@ var tasksCtrl = tasks.controller('TasksCtrl', ['$scope', '$location', function($
 var editTaskCtrl = tasks.controller('EditTaskCtrl', ['$scope', '$routeParams', 'Modernizr', function($scope, $routeParams, Modernizr) {
   $scope.taskId = $routeParams.id;
   $scope.task = {
-      id: "1",
-      name: "Task1",
-      description: "Description1",
-      completed: true,
-      assignees: [
-        {
-          name: "Antonio"
-        },
-        {
-          name: "Pablo"
-        },
-        {
-          name: "Samer"
-        },
-        {
-          name: "Juan"
-        }
-      ]
-    };
+    id: "1",
+    name: "Task1",
+    description: "Description1",
+    completed: true,
+    assignees: [
+      {
+        name: "Antonio"
+      },
+      {
+        name: "Pablo"
+      },
+      {
+        name: "Samer"
+      },
+      {
+        name: "Juan"
+      }
+    ]
+  };
 
   $scope.getTask = function(){
     // TODO use backend
@@ -114,18 +114,85 @@ var editTaskCtrl = tasks.controller('EditTaskCtrl', ['$scope', '$routeParams', '
       name: "Laura"
     }
   ];
+
   // //TODO backend
   $scope.reminders = [
     {
-        date : new Date() //.parse("November 1, 2014 10:15 AM")
+      id:"reminder1",
+      date : new Date()
     },
-          
-    {date : new Date()//.parse("November 12, 2014 11:15 PM")
+    
+    {
+      id:"reminder2",
+      date : new Date()//.parse("November 12, 2014 11:15 PM")
     }
   ];
+  $scope.reminders[0].date.setFullYear(2014);  
+  $scope.reminders[0].date.setMonth(9);
+  $scope.reminders[0].date.setDate(8);
+  $scope.reminders[0].date.setHours(9);
+  $scope.reminders[0].date.setMinutes(30);
+  $scope.reminders[0].date.setSeconds(0);
+  $scope.reminders[0].date.setMilliseconds(0);
+  //this array has the reminders as retrieved from server so when they changes the reminder of the phone can be updated.
+  
+  var oldReminders = [];
+  for (var i = 0; i < $scope.reminders.length; i += 1){
+    oldReminders[$scope.reminders[i].id]= new Date($scope.reminders[i].date.getTime());
+  }
+
+  //TODO call this function on save form
+  $scope.onSave = function(){
+    // TODO save other stuff
+    
+    // saving reminders in phone
+    for(var i = 0; i < $scope.reminders.length; i += 1){
+      var r = $scope.reminders[i];      
+      if (r.date.getTime() !== oldReminders[r.id].getTime()){
+        var oldDate = new Date();
+        oldDate.setTime(oldReminders[r.id].getTime());
+        //FIXME delete event not working
+        $scope.deleteReminder(oldDate, $scope.getTask().name);         
+        $scope.addReminder(r.date, $scope.getTask().name);
+      }
+    }
+    // TODO save also reminders
+  };
+
+  $scope.addReminder = function(date, title){
+    var dateWnd = new window.Date();
+    dateWnd.setTime(date.getTime());
+	var success = function(message) {
+	};
+	var error = function(message) {
+	  alert("Error: " + JSON.stringify(message));
+	};
+    
+	var calOptions = window.plugins.calendar.getCalendarOptions(); // grab the defaults
+	calOptions.firstReminderMinutes = 0;
+    var comment = "pear2pear reminder";
+	window.plugins.calendar.createEventWithOptions(title , "", comment,
+                                                   dateWnd, dateWnd, calOptions, success, error);
+
+  };
+  
+  $scope.deleteReminder = function(date, title){
+    var success = function(message) {
+    };
+	var error = function(message) {
+	  alert("Error: " + message);
+	};
+	var dateWnd = new window.Date();
+    dateWnd.setTime(date.getTime());
+	window.plugins.calendar.deleteEvent(title, null, null, dateWnd, dateWnd,
+				                        success, error);
+
+  };
+
   $scope.getReminders = function(taskId, userId){
     return $scope.reminders;
   };
+
   $scope.assigSelect = {};
   $scope.assigSelect.assignees = [
     $scope.groupUsers[0],$scope.groupUsers[1],
