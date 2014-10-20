d6c28d8518c4b9a02c2012a27c37dbd68fe429b9
Antonio Tapiador del Dujo <atapiador@ucm.es>
2014-10-20 11:10:38 +0200

    Include l10n dir in dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -341,7 +341,8 @@ module.exports = function (grunt) {
             '*.html',
             'views/{,*/}*.html',
             'images/{,*/}*.{webp}',
-            'fonts/*'
+            'fonts/*',
+            'l10n/*'
           ]
         }, {
           expand: true,
