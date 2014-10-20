4158a8bca4bd0212fc2582b266956b174859af9c
Antonio Tapiador del Dujo <atapiador@ucm.es>
2014-10-20 11:58:29 +0200

    Fix image versioning

--- Gruntfile.js
+++ Gruntfile.js
@@ -234,7 +234,7 @@ module.exports = function (grunt) {
 
     // Performs rewrites based on filerev and the useminPrepare configuration
     usemin: {
-      html: ['<%= yeoman.dist %>/{,*/}*.html'],
+      html: ['<%= yeoman.dist %>/{,*/}*.html', '<%= yeoman.dist %>/views/**/*.html'],
       css: ['<%= yeoman.dist %>/styles/{,*/}*.css'],
       options: {
         assetsDirs: ['<%= yeoman.dist %>','<%= yeoman.dist %>/images']
