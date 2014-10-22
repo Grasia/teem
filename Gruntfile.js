e377f240d402073413dba0a2ac584e91fbef0376
Antonio Tenorio Fornés <antoniotenorio@ucm.es>
2014-10-22 13:37:22 +0200

    minor: ignore some backup files

--- Gruntfile.js
+++ Gruntfile.js
@@ -56,8 +56,8 @@ module.exports = function (grunt) {
           livereload: '<%= connect.options.livereload %>'
         },
         files: [
-          '<%= yeoman.app %>/{,*/}*.html',
-          '<%= yeoman.app %>/views/**/*.html',
+          '<%= yeoman.app %>/{,*/}*.html$',
+          '<%= yeoman.app %>/views/**/*.html$',
           '.tmp/styles/{,*/}*.css',
           '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
         ]
