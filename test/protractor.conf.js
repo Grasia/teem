184df52f3d9413ea1a5f459180f84600bd77997e
Antonio Tapiador del Dujo <atapiador@ucm.es>
2014-11-06 12:17:51 +0100

    Increase protractor timeout interval

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -1,5 +1,5 @@
 exports.config = {
-  allScriptsTimeout: 11000,
+  allScriptsTimeout: 90000,
 
   specs: [
     'e2e/*.js'
@@ -14,6 +14,6 @@ exports.config = {
   framework: 'jasmine',
 
   jasmineNodeOpts: {
-    defaultTimeoutInterval: 30000
+    defaultTimeoutInterval: 90000
   }
 };
