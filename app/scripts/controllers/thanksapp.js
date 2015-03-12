e1aa609daf78cd35c60815bed4587b64229a2508
Antonio Tenorio Fornés <antoniotenorio@ucm.es>
2015-03-12 18:19:50 +0100

    minor: fix link with origin

--- app/scripts/controllers/thanksapp.js
+++ app/scripts/controllers/thanksapp.js
@@ -27,6 +27,11 @@ angular.module('Pear2Pear')
         $scope.$apply();
       }
     };
+    
+    if (!location.origin) {
+      location.origin = location.protocol + '//' + location.host;
+    }
+
 
     $scope.$parent.hideNavigation = true;
     $scope.mode = $route.current.params['mode'];
@@ -144,8 +149,7 @@ angular.module('Pear2Pear')
 
     $scope.thanksappify = function (string){
       var re = new RegExp('(\<@)(\\w+)', 'g');
-      var domain = window.location.hostname;
-      return string.replace(re, '<a href= "' + domain + '/#/thanksapp/view/$2"><strong><i class="icon-thanksapplogo"></i>$2</strong></a>');
+      return string.replace(re, '<a href= "' + window.origin + '/#/thanksapp/view/$2"><strong><i class="icon-thanksapplogo"></i>$2</strong></a>');
     };
 
     $scope.thanksappMails = function (string, sender){
@@ -165,9 +169,9 @@ angular.module('Pear2Pear')
           else {
             body += 'Te han enviado un agradecimiento: "<i>';
           }
-          var domain = window.location.hostname;
+
           var s = string
-            .replace(re, '<a href= "' + domain + '/#/thanksapp/view/$2"><strong>$1$2</strong></a>');
+            .replace(re, '<a href= "' + window.origin + '/#/thanksapp/view/$2"><strong>$1$2</strong></a>');
           s = salut + body + s + '</i>".<br/><br/>puedes ver tus agradecimientos y activar tu cuenta en <a href="#/thanksapp/self/'+receiver+'">tu Thanksapp</a>.';
           rt.push(s);
         }
