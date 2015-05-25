f3f370b1ecd93fb3d17af3e000df01682c1d6bf0
Antonio Tapiador del Dujo <atapiador@ucm.es>
2015-05-25 11:45:48 +0200

    Fix pad controller

--- src/js/controllers/pad.js
+++ src/js/controllers/pad.js
@@ -17,5 +17,5 @@ angular.module('Pear2Pear')
       });
   }])
   .controller('PadCtrl', ['pear', '$scope', '$route', function(pear, $scope, $route){
-    $scope.project = pear.find($route.current.params.id);
+    $scope.project = pear.projects.find($route.current.params.id);
   }]);
