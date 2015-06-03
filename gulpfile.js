242e3fa0e3a888e81c9fd9bbe59e2d49e25add20
Antonio Tapiador del Dujo <atapiador@ucm.es>
2015-06-03 17:18:27 +0200

    Use ryanmullins-angular-hammer
    It is compatible with hammerjs 2.x

--- gulpfile.js
+++ gulpfile.js
@@ -29,7 +29,7 @@ var config = {
       './bower_components/angulartics/src/angulartics-piwik.js',
       './bower_components/angular-swellrt/angular-swellrt.js',
       './bower_components/hammerjs/hammer.js',
-      './bower_components/angular-hammer/angular-hammer.js'
+      './bower_components/ryanmullins-angular-hammer/angular.hammer.js'
 
     ],
 
