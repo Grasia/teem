f2f7718bf8a6cd4779cbba3c76e601ac56f57033
Antonio Tapiador del Dujo <atapiador@ucm.es>
2015-06-16 12:20:25 +0200

    Chatpad e2e tests

---
+++ test/e2e/chatpad.js
@@ -0,0 +1,73 @@
+'use strict';
+
+/* https://github.com/angular/protractor/blob/master/docs/toc.md */
+
+describe('Pear2Pear', function() {
+
+  browser.get('index.html');
+
+  it('should automatically redirect to /session/new when location hash/fragment is empty', function() {
+    expect(browser.getLocationAbsUrl()).toMatch('/session/new');
+  });
+
+
+  describe('session/new', function() {
+
+    beforeEach(function() {
+      browser.get('index.html#/session/new');
+    });
+
+
+    it('should render session/new when user navigates to /session/new', function() {
+      expect(element.all(by.css('[ng-view] input#login')).first()).toBeDefined();
+    });
+
+  });
+
+  describe('chatpad', function() {
+    beforeAll(function() {
+      browser.get('index.html');
+    });
+
+    it('should use the chadpad', function() {
+      var timeout = 30000;
+
+      element(by.css('input#login')).sendKeys('Test');
+      element(by.css('.login-form button')).click();
+
+      var projectList = by.css('.projects');
+
+      browser.wait(function() {
+        return browser.isElementPresent(projectList);
+      }, timeout);
+
+      // Wait until pear has loaded the projects
+      browser.wait(function() {
+        return element(projectList).evaluate('projects') !== null;
+      }, timeout);
+
+      var newButton = by.css('.new-project a');
+      element(newButton).click();
+
+      var editTitle = by.css('.project-title input');
+
+      browser.wait(function() {
+        return browser.isElementPresent(editTitle);
+      }, timeout);
+
+      element(editTitle).sendKeys('Testing');
+      element(by.css('#pad textarea')).sendKeys('Grow your community with Pear2Pear');
+
+      element(by.css('a.nav-chat')).click();
+
+      var chatText = 'This is a nice opportunity to discuss about testing';
+
+      element(by.css('.chat-send textarea')).sendKeys(chatText);
+      element(by.css('.chat-input-button')).click();
+
+      expect(element.all(by.css('.chat-message-text')).last().getText())
+        .toEqual(chatText);
+
+    });
+  });
+});
