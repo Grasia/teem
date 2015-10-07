72b48128d52d32426d5077bf73ea6c950a0eac1e
Antonio Tenorio Fornés <antoniotenorio@ucm.es>
2015-10-07 11:44:55 +0200

    fix edition count method

--- src/js/services/profilesService.js
+++ src/js/services/profilesService.js
@@ -13,8 +13,8 @@ angular.module('Pear2Pear')
 
     Profile.prototype.getPadEditionCount = function(proj){
       var lastVisit =
-        (this.lastProjectVisit[project.id])?
-        new Date(this.lastProjectVisit[project.id]):new Date(0);
+        (this.lastProjectVisit[proj.id])?
+        new Date(this.lastProjectVisit[proj.id]):new Date(0);
 
       if (lastVisit.getTime() < proj.pad.lastmodtime){
         return 0;
