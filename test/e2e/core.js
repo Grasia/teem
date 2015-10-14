22060109226954a912a8ca0c5e2ccdf9958efc10
Antonio Tenorio Fornés <antoniotenorio@ucm.es>
2015-10-14 22:29:32 +0200

    fix tests for bigger screens

--- test/e2e/core.js
+++ test/e2e/core.js
@@ -38,11 +38,12 @@ describe('Pear2Pear', function() {
       var communitySearchInput = by.css('.community-search input');
       var toggleCommunitySearchBtn = by.css('.sidebar-toggle');
 
-      browser.wait(function() {
-        return browser.isElementPresent(toggleCommunitySearchBtn);
-      }, timeout);
+      element(communitySearchInput).isDisplayed().then(function(displayed){
+        if (!displayed){
+          element(toggleCommunitySearchBtn).click();
+        }
+      });
 
-      element(toggleCommunitySearchBtn).click();
 
       browser.wait(function() {
         return element(communitySearchInput).isDisplayed() &&
