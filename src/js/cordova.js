97f346c585a727806718db2b02bed6a9ca5ec5c9
Antonio Tapiador del Dujo <atapiador@ucm.es>
2015-11-10 17:43:24 +0100

    Update application cache on device resume

--- src/js/cordova.js
+++ src/js/cordova.js
@@ -3,7 +3,25 @@
 // Load only within android app: cordova=android
 if (window.location.search.search('cordova') > 0) {
   (function(d, script) {
+    // When cordova is loaded
+    function onLoad() {
+      d.addEventListener('deviceready', onDeviceReady, false);
+    }
+
+    // Device APIs are available
+    function onDeviceReady() {
+      d.addEventListener('resume', onResume, false);
+    }
+
+    // When device comes to foreground
+    function onResume() {
+      if (window.applicationCache) {
+        window.applicationCache.update();
+      }
+    }
+
     script = d.createElement('script');
+    script.onload = onLoad;
     script.type = 'text/javascript';
     script.src = 'js/cordova/cordova.js';
     d.getElementsByTagName('head')[0].appendChild(script);
