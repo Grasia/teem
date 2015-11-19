cfa4b1ef097d146d4cb2f13ad2f596a3a6bef431
Antonio Tenorio Fornés <antoniotenorio@ucm.es>
2015-11-19 18:15:43 +0100

    add cordova push plugin for notifications

--- cordova/platforms/android/assets/www/cordova_plugins.js
+++ cordova/platforms/android/assets/www/cordova_plugins.js
@@ -1,15 +1,22 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
+        "file": "plugins/phonegap-plugin-push/www/push.js",
+        "id": "phonegap-plugin-push.PushNotification",
+        "pluginId": "phonegap-plugin-push",
+        "clobbers": [
+            "PushNotification"
+        ]
+    },
+    {
         "file": "plugins/cordova-plugin-whitelist/whitelist.js",
         "id": "cordova-plugin-whitelist.whitelist",
+        "pluginId": "cordova-plugin-whitelist",
         "runs": true
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
-{
-    "cordova-plugin-whitelist": "1.1.1-dev"
-}
+{}
 // BOTTOM OF METADATA
 });
