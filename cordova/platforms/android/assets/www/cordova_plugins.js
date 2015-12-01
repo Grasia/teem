b33f03a1dc40b84c3544fc81727132d7b3140fb6
Antonio Tenorio Fornés <antoniotenorio@ucm.es>
2015-12-01 10:23:42 +0100

    plugin order change, (automatically changed after some cordova operations)

--- cordova/platforms/android/assets/www/cordova_plugins.js
+++ cordova/platforms/android/assets/www/cordova_plugins.js
@@ -1,18 +1,18 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "pluginId": "cordova-plugin-whitelist",
+        "runs": true
+    },
+    {
         "file": "plugins/phonegap-plugin-push/www/push.js",
         "id": "phonegap-plugin-push.PushNotification",
         "pluginId": "phonegap-plugin-push",
         "clobbers": [
             "PushNotification"
         ]
-    },
-    {
-        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
-        "id": "cordova-plugin-whitelist.whitelist",
-        "pluginId": "cordova-plugin-whitelist",
-        "runs": true
     }
 ];
 module.exports.metadata = 
