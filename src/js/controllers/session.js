9b9559787275315309d329fa74527a2ad4a9cd52
Antonio Tapiador del Dujo <atapiador@ucm.es>
2015-12-17 18:25:45 +0100

    Clean legacy prototypes code
    @atfornes heads up!
    
    I created the tag "prototypes" pointing to the last commit of the
    prototype series. We should be able to recover code from there if needed

--- src/js/controllers/session.js
+++ src/js/controllers/session.js
@@ -74,28 +74,4 @@ angular.module('Teem')
         $location.path('/communities');
       }
     }
-
-    $scope.userData = function () {
-      _paq.push(['appendToTrackingUrl', 'new_visit=1']);
-      _paq.push(["deleteCookies"]);
-      _paq.push(['setCustomVariable', 1, 'gender', $scope.user.gender, 'visit']);
-      _paq.push(['setCustomVariable', 2, 'age', $scope.user.age, 'visit']);
-      _paq.push(['setCustomVariable', 3, 'role', $scope.user.role, 'visit']);
-      _paq.push(['setCustomVariable', 4, 'tech', $scope.user.tech, 'visit']);
-      _paq.push(['setCustomVariable', 5, 'community', $scope.user.community, 'visit']);
-
-      // tracker.storeCustomVariablesInCookie();
-      _paq.push(['trackEvent', 'UserQuestionaire', 'answer']);
-      _paq.push(['trackPageView']);
-      if ($route.current.params['redirect']) {
-         var redirect = $route.current.params['redirect'];
-        $location.path(redirect);
-      }
-      else if ($route.current.params['predirect']) {
-        window.location.href =  $route.current.params['predirect'];
-      }
-      else {
-        $location.path('/timeline');
-      }
-    };
   }]);
