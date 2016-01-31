7adf435f8b682929442b57af390cea928bdc3896
David Llop <llop-no-spam-please@protonmail.com>
2016-01-31 23:42:45 +0100

    Added register, forgotten password, and recover password forms.
    They aren't doing anything yet.
    Some checks should be implemented.
    These URLs have been added:
      * session/login
      * session/register
      * session/forgotten_password
      * session/recover_password

--- src/js/controllers/session.js
+++ src/js/controllers/session.js
@@ -10,7 +10,7 @@
 angular.module('Teem')
   .config(['$routeProvider', function($routeProvider) {
     $routeProvider.
-      when('/session/new', {
+      when('/session/:form', {
         templateUrl: 'session/new.html',
         controller:'SessionCtrl'
       });
@@ -20,17 +20,24 @@ angular.module('Teem')
     function($scope, $location, $route, SessionSvc, $timeout, SharedState) {
     $scope.session = {};
 
-    $scope.loginRegexp = new RegExp('^[a-zA-Z0-9\.]+$');
-
     $scope.user = {
       nick : ''
     };
 
-    $scope.login = function() {
+    var validateFormName = function(form) {
+      var forms = ['login', 'register', 'forgotten_password', 'recover_password'];
+      var isValid = forms.reduce(function(last, current) {
+        return last || form == current;
+      }, false);
+      return isValid? form : 'login';
+    }
+
+    var login = function() {
+      var fields = $scope.current().values;
       var startSession = function(){
         // TODO change password when register is available
         SessionSvc.startSession(
-          $scope.user.nick, SessionSvc.users.password,
+          fields.nick, SessionSvc.users.password,
           function(){
             $timeout(function(){
               SharedState.turnOff('shouldLoginSharedState');
@@ -41,6 +48,98 @@ angular.module('Teem')
           }
         );
       };
-      SessionSvc.registerUser($scope.user.nick, '$password$', startSession, startSession);
+      //SessionSvc.registerUser(fields.nick, fields.password, startSession, startSession);
+      console.log("login", fields);
     };
+
+    var register = function() {
+      // TODO
+      console.log("register", $scope.current().values);
+    }
+
+    var forgotten_password = function() {
+      // TODO
+      console.log("forgotten", $scope.current().values);
+    }
+
+    var recover_password = function() {
+      // TODO
+      console.log("recover", $scope.current().values);
+    }
+
+    $scope.form = {
+      current: validateFormName($route.current.params.form),
+      login: {
+        fields: {
+          nick: {
+            name: "nick",
+            pattern: new RegExp('^[a-zA-Z0-9\.]+$'),
+            model_options: "{debounce: 250}",
+            required: true,
+            autofocus: true
+
+          },
+          password: {
+            name: "password",
+            type: "password",
+            required: true
+          }
+        },
+        submit: login
+      },
+      register: {
+        fields: [
+          {
+            name: "nick",
+            required: true,
+            autofocus: true
+          },
+          {
+            name: "password",
+            type: "password",
+            required: true
+          },
+          {
+            name: "password_repeat",
+            type: "password",
+            required: true
+          },
+          {
+            name: "email",
+            type: "email",
+            required: false
+          }
+        ],
+        submit: register
+      },
+      forgotten_password: {
+        fields: [
+          {
+            name: "email",
+            type: "email",
+            required: true
+          }
+        ],
+        submit: forgotten_password
+      },
+      recover_password: {
+        fields: [
+          {
+            name: "password",
+            type: "password",
+            autofocus: true
+          },
+          {
+            name: "password_repeat",
+            type: "password"
+          }
+        ],
+        submit: recover_password
+      }
+    };
+
+    $scope.current = function() {
+      return $scope.form[$scope.form.current];
+    };
+
   }]);
--- src/js/directives/shouldLoginModal.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('Teem')
-  .directive('shouldLoginModal', function() {
-    return {
-      templateUrl: 'should-login-modal.html'
-    };
-  });
