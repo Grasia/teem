7c601bbddd50bbfe4b5f73dfd9865ebd5d59d4b7
Antonio Tenorio Fornés <antoniotenorio@ucm.es>
2016-02-19 22:04:56 +0000

    fix form validation problems

--- src/js/directives/formValidation.js
+++ src/js/directives/formValidation.js
@@ -8,8 +8,9 @@ angular.module('Teem')
   return {
     require: 'ngModel',
     link: function (scope, elm, attrs, ctl) {
-      scope.$watch(attrs['validationError'], function (errorMsg) {
-        elm[0].setCustomValidity(errorMsg);
+      scope.$watch(attrs.validationError, function (errorMsg) {
+        console.log(errorMsg);
+        elm[0].setCustomValidity(errorMsg || '');
         ctl.$setValidity('validationError', !errorMsg);
       });
     }
