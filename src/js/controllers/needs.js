6ccf7ac27cc0f3f9071f5842abbbb87059af11ca
Antonio Tapiador del Dujo <atapiador@ucm.es>
2016-02-29 12:09:06 +0100

    Fix call to Session.users.loggedIn

--- src/js/controllers/needs.js
+++ src/js/controllers/needs.js
@@ -108,7 +108,7 @@ angular.module('Teem')
           scope.hour = needsCtrl.hour;
 
           scope.newComments = function(need){
-            if (!need.comments || !scope.project || SessionSvc.users.isLoggedIn()){
+            if (!need.comments || !scope.project || SessionSvc.users.loggedIn()){
               return false;
             }
 
@@ -118,7 +118,7 @@ angular.module('Teem')
           };
 
           scope.isNewNeed = function(need){
-            if (!scope.project || !SessionSvc.users.isLoggedIn()){
+            if (!scope.project || !SessionSvc.users.loggedIn()){
               return false;
             }
             var prevAccess = new Date(scope.project.getTimestampAccess().needs.prev);
