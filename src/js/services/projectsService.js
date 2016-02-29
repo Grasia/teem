156cb8bf06c5e804ceb94cb6227adbd4d092a905
Antonio Tapiador del Dujo <atapiador@ucm.es>
2016-02-29 16:00:17 +0100

    Refactor ProjectsSvc with ES6 clases
    yeah

--- src/js/services/projectsService.js
+++ src/js/services/projectsService.js
@@ -6,251 +6,257 @@ angular.module('Teem')
   function(swellRT, $q, $timeout, base64, SessionSvc, SwellRTCommon){
 
     // class that expose only read methods of the project object
-    var ReadOnlyProject = function(){};
-
-    ReadOnlyProject.prototype.getTimestampAccess = function() {
-      var access;
-
-      if (this.lastAccesses === undefined) {
-        this.lastAccesses = [];
-      }
-
-      angular.forEach(this.lastAccesses, function(a) {
-        if (a.user === SessionSvc.users.current()) {
-          access = a;
+    class ProjectReadOnly {
+      constructor (val) {
+        if (val) {
+          for (var k in val.root){
+            if (val.root.hasOwnProperty(k)){
+              this[k] = val.root[k];
+            }
+          }
         }
-      });
-
-      if (! access) {
-        access = {
-          user: SessionSvc.users.current()
-        };
-
-        this.lastAccesses.push(access);
       }
 
-      return access;
-    };
+      getTimestampAccess () {
+        var access;
 
-        // section in {'chat', 'pad', 'needs'}
-    ReadOnlyProject.prototype.lastChange = function(section){
-      if (section === undefined) {
-        //cast to Date
-        return new Date(Math.max(
-          this.lastChange('chat'),
-          this.lastChange('pad'),
-          this.lastChange('needs')
-        ));
-      } else {
-        switch (section) {
+        if (this.lastAccesses === undefined) {
+          this.lastAccesses = [];
+        }
 
-        case 'chat':
-          if (this.chat && this.chat.length > 0) {
-            return new Date(this.chat[this.chat.length -1].time);
-          } else {
+        angular.forEach(this.lastAccesses, function(a) {
+          if (a.user === SessionSvc.users.current()) {
+            access = a;
+          }
+        });
+
+        if (! access) {
+          access = {
+            user: SessionSvc.users.current()
+          };
+
+          this.lastAccesses.push(access);
+        }
+
+        return access;
+      }
+
+      // section in {'chat', 'pad', 'needs'}
+      lastChange (section) {
+        if (section === undefined) {
+          //cast to Date
+          return new Date(Math.max(
+            this.lastChange('chat'),
+            this.lastChange('pad'),
+            this.lastChange('needs')
+          ));
+        } else {
+          switch (section) {
+
+          case 'chat':
+            if (this.chat && this.chat.length > 0) {
+              return new Date(this.chat[this.chat.length -1].time);
+            } else {
+              return new Date(0);
+            }
+            break;
+
+          case 'pad':
+            return new Date(this.pad.lastmodtime) || new Date(0);
+
+          case 'needs':
+            var lastChange = new Date(0);
+            angular.forEach(this.needs, function(n){
+              lastChange = Math.max(
+                lastChange,
+                new Date(n.time),
+                (function() {
+                  var lastComment = new Date(0);
+                  angular.forEach(n.comments, function(c){
+                    lastComment = Math.max(lastComment, new Date(c.time));
+                  });
+                  return lastComment;
+                }())
+              );
+            });
+            // cast to Date
+            return new Date(lastChange);
+
+          default:
             return new Date(0);
           }
-          break;
-
-        case 'pad':
-          return new Date(this.pad.lastmodtime) || new Date(0);
-
-        case 'needs':
-          var lastChange = new Date(0);
-          angular.forEach(this.needs, function(n){
-            lastChange = Math.max(
-              lastChange,
-              new Date(n.time),
-              (function() {
-                var lastComment = new Date(0);
-                angular.forEach(n.comments, function(c){
-                  lastComment = Math.max(lastComment, new Date(c.time));
-                });
-                return lastComment;
-              }())
-            );
-          });
-          // cast to Date
-          return new Date(lastChange);
-
-        default:
-          return new Date(0);
         }
       }
-    };
 
-    // section in {'chat', 'pad', 'needs'}
-    // pos in {'last','prev'}
-    ReadOnlyProject.prototype.lastAccess = function(section, pos) {
-      var access;
+      // section in {'chat', 'pad', 'needs'}
+      // pos in {'last','prev'}
+      lastAccess (section, pos) {
+        var access;
 
-      angular.forEach(this.lastAccesses || [], function(a) {
-        if (a.user === SessionSvc.users.current()) {
-          access = a;
+        angular.forEach(this.lastAccesses || [], function(a) {
+          if (a.user === SessionSvc.users.current()) {
+            access = a;
+          }
+        });
+
+        if (!pos) {
+          pos = 'last';
         }
-      });
 
-      if (!pos) {
-        pos = 'last';
+        return (access && access[section] && access[section][pos] ? new Date(access[section][pos]) : new Date(0));
       }
 
-      return (access && access[section] && access[section][pos] ? new Date(access[section][pos]) : new Date(0));
+      newMessagesCount () {
+        var access = this.lastAccess('chat');
 
-    };
+        if (this.chat.length > 0){
+          var i = this.chat.length - 1;
 
-    ReadOnlyProject.prototype.newMessagesCount = function() {
-      var access = this.lastAccess('chat');
-
-      if (this.chat.length > 0){
-        var i = this.chat.length - 1;
-
-        while (i > -1 && (new Date(this.chat[i].time) > access)) {
-          i --;
+          while (i > -1 && (new Date(this.chat[i].time) > access)) {
+            i --;
+          }
+          return this.chat.length - 1 - i;
+        } else {
+          return 0;
         }
-        return this.chat.length - 1 - i;
-      } else {
-        return 0;
       }
-    };
 
-    ReadOnlyProject.prototype.padEditionCount = function() {
-      if (this.lastAccess('pad').getTime() < this.pad.lastmodtime) {
-        return 1;
-      } else {
-        return 0;
+      padEditionCount () {
+        if (this.lastAccess('pad').getTime() < this.pad.lastmodtime) {
+          return 1;
+        } else {
+          return 0;
+        }
       }
-    };
 
-    ReadOnlyProject.prototype.isSupporter = function(user){
-      if (!user){
+      isSupporter (user) {
+        if (!user){
+          if (! SessionSvc.users.loggedIn()) {
+            return false;
+          }
+
+          user = SessionSvc.users.current();
+        }
+        return this.supporters.indexOf(user) > -1;
+      }
+
+      isContributor (user) {
+        if (! user){
+          if (! SessionSvc.users.loggedIn()) {
+            return false;
+          }
+
+          user = SessionSvc.users.current();
+        }
+        return this.contributors.indexOf(user) > -1;
+      }
+    }
+
+    class Project extends ProjectReadOnly {
+
+      addContributor (user) {
+        if (!user){
+          user = SessionSvc.users.current();
+        }
+        if (user && this.contributors.indexOf(user) < 0){
+          this.contributors.push(user);
+        }
+      }
+
+      setShareMode (shareMode) {
+        this.shareMode = shareMode;
+      }
+
+      /*
+       * Record when the user had her last and previous access to one project section
+       */
+      setTimestampAccess (section, overridePrev) {
+        if (! this.isContributor()) {
+          return;
+        }
+
+        var timestamp = this.getTimestampAccess()[section];
+
+        if (timestamp === undefined) {
+          timestamp = {};
+        }
+
+        if (timestamp instanceof Date) {
+          timestamp = {
+            last: timestamp
+          };
+        }
+
+        if(! overridePrev){
+          timestamp.prev = timestamp.last || (new Date()).toJSON();
+        } else {
+          timestamp.prev = (new Date()).toJSON();
+        }
+
+        timestamp.last = (new Date()).toJSON();
+
+        this.getTimestampAccess()[section] = timestamp;
+      }
+
+      toggleSupport () {
+        if (SessionSvc.users.current() === null) {
+          return;
+        }
+        var index = this.supporters.indexOf(SessionSvc.users.current());
+
+        if (index > -1) {
+          this.supporters.splice(index, 1);
+        } else {
+          this.supporters.push(SessionSvc.users.current());
+        }
+      }
+
+      removeContributor (user) {
+        if (!user){
+          user = SessionSvc.users.current();
+        }
+
+        this.contributors.splice(
+          this.contributors.indexOf(user),
+          1);
+      }
+
+      toggleContributor () {
         if (! SessionSvc.users.loggedIn()) {
-          return false;
+          return;
         }
 
-        user = SessionSvc.users.current();
-      }
-      return this.supporters.indexOf(user) > -1;
-    };
+        var user = SessionSvc.users.current();
 
-    ReadOnlyProject.prototype.isContributor = function(user){
-      if (! user){
-        if (! SessionSvc.users.loggedIn()) {
-          return false;
+        if (this.isContributor(user)) {
+          this.removeContributor(user);
+        } else {
+          this.addContributor(user);
         }
-
-        user = SessionSvc.users.current();
-      }
-      return this.contributors.indexOf(user) > -1;
-    };
-
-    var Project = function(){};
-
-    //inheritance
-    Project.prototype = Object.create(ReadOnlyProject.prototype);
-
-    Project.prototype.addContributor = function(user) {
-      if (!user){
-        user = SessionSvc.users.current();
-      }
-      if (user && this.contributors.indexOf(user) < 0){
-        this.contributors.push(user);
-      }
-    };
-
-    Project.prototype.setShareMode = function(shareMode){
-      this.shareMode = shareMode;
-    };
-
-    /*
-     * Record when the user had her last and previous access to one project section
-     */
-    Project.prototype.setTimestampAccess = function(section, overridePrev){
-      if (! this.isContributor()) {
-        return;
       }
 
-      var timestamp = this.getTimestampAccess()[section];
-
-      if (timestamp === undefined) {
-        timestamp = {};
-      }
-
-      if (timestamp instanceof Date) {
-        timestamp = {
-          last: timestamp
-        };
-      }
-
-      if(! overridePrev){
-        timestamp.prev = timestamp.last || (new Date()).toJSON();
-      } else {
-        timestamp.prev = (new Date()).toJSON();
-      }
-
-      timestamp.last = (new Date()).toJSON();
-
-      this.getTimestampAccess()[section] = timestamp;
-
-    };
-
-    Project.prototype.toggleSupport = function(){
-      if (SessionSvc.users.current() === null) {
-        return;
-      }
-      var index = this.supporters.indexOf(SessionSvc.users.current());
-
-      if (index > -1) {
-        this.supporters.splice(index, 1);
-      } else {
-        this.supporters.push(SessionSvc.users.current());
-      }
-    };
-
-    Project.prototype.removeContributor = function(user) {
-      if (!user){
-        user = SessionSvc.users.current();
-      }
-
-      this.contributors.splice(
-        this.contributors.indexOf(user),
-        1);
-    };
-
-    Project.prototype.toggleContributor = function(){
-      if (! SessionSvc.users.loggedIn()) {
-        return;
-      }
-
-      var user = SessionSvc.users.current();
-
-      if (this.isContributor(user)) {
-        this.removeContributor(user);
-      } else {
-        this.addContributor(user);
-      }
-    };
-
-    Project.prototype.addChatMessage = function(message){
-      this.chat.push({
+      addChatMessage (message) {
+        this.chat.push({
           text: message,
           who: SessionSvc.users.current(),
           time: (new Date()).toJSON()
         });
-      this.addContributor();
-      this.setTimestampAccess('chat', true);
-    };
-
-    Project.prototype.addNeedComment = function(need, comment){
-      if (!need.comments){
-        need.comments = [];
+        this.addContributor();
+        this.setTimestampAccess('chat', true);
       }
-      need.comments.push({
-        text: comment,
-        time: (new Date()).toJSON(),
-        author: SessionSvc.users.current()
-      });
-      this.setTimestampAccess('needs', true);
-    };
+
+      addNeedComment (need, comment) {
+        if (!need.comments){
+          need.comments = [];
+        }
+        need.comments.push({
+          text: comment,
+          time: (new Date()).toJSON(),
+          author: SessionSvc.users.current()
+        });
+        this.setTimestampAccess('needs', true);
+      }
+    }
 
     // Service functions //
 
@@ -304,13 +310,7 @@ angular.module('Teem')
         SwellRT.query(query, function(result) {
           angular.forEach(result.result, function(val){
 
-            var v = new ReadOnlyProject();
-
-            for (var k in val.root){
-              if (val.root.hasOwnProperty(k)){
-                v[k] = val.root[k];
-              }
-            }
+            var v = new ProjectReadOnly(val);
 
             res.push(v);
           });
