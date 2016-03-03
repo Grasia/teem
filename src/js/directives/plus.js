425c7bbc25e6f22161bfb8474dd677fe5bb4ce77
Antonio Tapiador del Dujo <atapiador@ucm.es>
2016-03-03 11:54:11 +0100

    Plus in communities

---
+++ src/js/directives/plus.js
@@ -0,0 +1,42 @@
+'use strict';
+
+angular.module('Teem')
+  .directive('plus', function() {
+    return {
+      scope: {
+        community: '=plusCommunity',
+        type: '@plusType'
+      },
+      controller: [
+      '$scope', 'SessionSvc', 'CommunitiesSvc', 'ProjectsSvc', '$location',
+      function($scope, SessionSvc, CommunitiesSvc, ProjectsSvc, $location) {
+
+        SessionSvc.onLoad(function(){
+          $scope.create = function () {
+            SessionSvc.loginRequired(function() {
+              let params = {};
+
+              if ($scope.type === 'community') {
+
+                CommunitiesSvc.create({}, function(c) {
+                  $location.path('/communities/' + c.urlId).search('form', 'new');
+                });
+
+              } else if ($scope.type === 'project') {
+
+                if ($scope.community) {
+                  params.communityId = $scope.community.id;
+                }
+
+                ProjectsSvc.create(params, function(p) {
+                  $location.path('/projects/' + p.urlId).search('form', 'new');
+                });
+              }
+            });
+          };
+        });
+
+      }],
+      templateUrl: 'plus.html'
+    };
+  });
