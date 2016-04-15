e4e3fbf101e23e6cf36179a511a65e86cf016608
David Llop <llop-no-spam-please@protonmail.com>
2016-04-15 21:47:25 +0200

    Add format buttons to pad

--- src/js/directives/pad.js
+++ src/js/directives/pad.js
@@ -21,8 +21,10 @@ angular.module('Teem')
             editting: false
           };
 
+          var buttons = ['bold', 'italic', 'underline', 'strikethrough'];
+
           function editOn () {
-            $scope.pad.editting = true;
+            //$scope.pad.editting = true;
 
             SessionSvc.showSaving = true;
 
@@ -37,23 +39,51 @@ angular.module('Teem')
             $timeout();
           }
 
-          $scope.padReady = function() {
+          $scope.padReady = function(editor) {
             // FIXME
             // SwellRT editor is created with .wave-editor-off
             // Should use .wave-editor-on when SwellRT editor callback is available
             // https://github.com/P2Pvalue/swellrt/issues/84
-            var editor = angular.element(document.getElementById('pad').children[0]);
+            var editorElement = angular.element(document.getElementById('pad').children[0]);
+
+            var annotationMap = {
+              bold: 'style/fontWeight=bold',
+              italic: 'style/fontStyle=italic',
+              underline: 'style/textDecoration=underline',
+              strikethrough: 'style/textDecoration=line-through'
+            };
+
+            $scope.buttons = {};
+            buttons.forEach(btn => $scope.buttons[btn] = false);
+
+            editor.onSelectionChanged(function(annotations) {
+              for (let btn of buttons) {
+                let [key, val] = annotationMap[btn].split('=');
+                $scope.buttons[btn] = (annotations[key] === val);
+              }
+              $timeout();
+            });
+
+            $scope.annotate = function(btn) {
+              let [key, val] = annotationMap[btn].split('=');
+              $scope.buttons[btn] = !$scope.buttons[btn];
+              if (!$scope.buttons[btn]) {
+                val = null;
+              }
+              editor.setAnnotation(key, val);
+              editorElement.focus();
+            };
 
             $scope.$watch(function() {
-              return editor.attr('class');
+              return editorElement.attr('class');
             },
             function(newClass) {
               if (newClass === 'wave-editor-on') {
-                editor.
+                editorElement.
                   on('focus', editOn).
                   on('blur', editOff);
               } else if (newClass === 'wave-editor-off') {
-                editor.
+                editorElement.
                   off('focus', editOn).
                   off('blur', editOff);
               }
