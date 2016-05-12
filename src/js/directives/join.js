12292840aa49c34a87eb7ee654e4cdc0651f4047
Antonio Tapiador del Dujo <atapiador@ucm.es>
2016-05-12 10:04:11 +0200

    Restore analytics events
    Closes #238

--- src/js/directives/join.js
+++ src/js/directives/join.js
@@ -4,14 +4,19 @@ angular.module('Teem')
   .directive('join', function() {
     return {
       controller: [
-      '$scope', '$element', '$attrs', 'SessionSvc', '$timeout',
-      function($scope, $element, $attrs, SessionSvc, $timeout) {
+      '$scope', '$element', '$attrs', 'SessionSvc', '$timeout', '$analytics',
+      function($scope, $element, $attrs, SessionSvc, $timeout, $analytics) {
         $scope.joinIcon = $attrs.joinIcon;
         $scope.joinCopyOn  = $attrs.joinCopyOn;
         $scope.joinCopyOff = $attrs.joinCopyOff;
 
         $element.on('click', function() {
           SessionSvc.loginRequired($scope, function() {
+
+            if (!$scope.project.isContributor()) {
+              $analytics.eventTrack('Join project');
+            }
+
             $scope.project.toggleContributor();
             $timeout();
           });
--- src/js/services/new-form.js
+++ src/js/services/new-form.js
@@ -35,6 +35,7 @@ angular.module('Teem')
       scope = s;
       objectName = o;
 
+      scope.newFormObjectName = o;
       Object.assign(scope, scopeFn);
     }
 
