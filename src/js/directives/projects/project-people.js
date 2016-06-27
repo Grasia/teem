f8dd5b71e754805906cf8722645751d1ddcc42db
David Llop <llop-no-spam-please@protonmail.com>
2016-06-27 08:49:58 +0200

    Tweaks in project desktop views

--- src/js/directives/projects/project-people.js
+++ src/js/directives/projects/project-people.js
@@ -4,10 +4,6 @@ angular.module('Teem')
   .directive('projectPeople', function() {
     return {
       scope: true,
-      controller: [
-        'SessionSvc', '$scope', '$location',
-        function (SessionSvc, $scope, $location) {
-      }],
       templateUrl: 'projects/project-people.html'
     };
   });
