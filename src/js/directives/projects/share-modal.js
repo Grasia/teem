fd0782060508648136fd626012c35ef9d793f3a8
Antonio Tenorio Fornés <antoniotenorio@ucm.es>
2016-06-29 12:48:28 +0200

    Share dialog updated, invite to communities updated

--- src/js/directives/projects/share-modal.js
+++ src/js/directives/projects/share-modal.js
@@ -3,7 +3,32 @@
 angular.module('Teem')
   .directive('projectsShareModal', function() {
     return {
-      controller: function() {},
+      controller: ['$scope', '$filter', function($scope, $filter) {
+        $scope.share = {
+          options: [
+            {id: 'link', title: 'project.share.info.link'},
+            {id: 'public', title: 'project.share.info.public'}
+          ],
+          config: {
+            valueField: 'id',
+            labelField: 'title',
+            sortField: 'title',
+            maxItems: 1,
+            render: {
+              item: function(item, escape) {
+                return '<div translate>' +
+                  $filter('translate')(escape(item.title)) +
+                  '</div>';
+              },
+              option: function(item, escape) {
+                return '<div translate>' +
+                  $filter('translate')(escape(item.title)) +
+                  '</div>';
+              }
+            },
+          },
+        };
+      }],
       templateUrl: 'projects/share-modal.html'
     };
   });
