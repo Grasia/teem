51c616704333f5aec021cbd0aa93e8bd7981c8ac
Antonio Tapiador del Dujo <atapiador@ucm.es>
2016-07-11 17:27:40 +0200

    Fix guest specs

--- test/e2e/guest/project.js
+++ test/e2e/guest/project.js
@@ -2,23 +2,20 @@
 
 var projectPages = require('./../pages/project'),
     sessionPages = require('./../pages/session'),
-    MenuPage = require('./../pages/menu'),
     projectPage = new projectPages.ProjectPage(),
     loginPage = new sessionPages.Login(),
-    menu = new MenuPage();
+    logoutPage = new sessionPages.Logout();
 
 describe('Guest user', () => {
 
   beforeAll(() => {
-    browser.get('/session/logout');
+    logoutPage.get();
   });
 
   it('should be able to see a project with the core user as participant', () => {
     browser.get(global.defaultProject.url);
 
     browser.wait(protractor.ExpectedConditions.visibilityOf(projectPage.titleEl));
-
-    //expect(menu.isLoggedIn()).toBeFalsy();
 
     expect(projectPage.getTitle()).toBe(global.defaultProject.title);
 
