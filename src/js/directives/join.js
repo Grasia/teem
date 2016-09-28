453df3de1547f36c607a366dff03019769b3919d
Antonio Tapiador del Dujo <atapiador@ucm.es>
2016-09-28 12:15:49 +0200

    Restore join functionality without a modal

--- src/js/directives/join.js
+++ src/js/directives/join.js
@@ -14,10 +14,18 @@ angular.module('Teem')
         $element.on('click', function() {
 
           if (!$scope.project.isParticipant()){
-            SharedState.set('modalSharedState', { name: 'join' });
+            if ($attrs.joinModal) {
+              SharedState.set('modalSharedState', { name: 'join' });
+            } else {
+              SessionSvc.loginRequired($scope, function() {
+                $scope.project.addParticipant();
+              }, undefined, $scope.project.synchPromise());
+            }
+
             $timeout();
             $analytics.eventTrack('Join project', {});
           } else {
+
             SessionSvc.loginRequired($scope, function() {
               $scope.project.removeParticipant();
             }, undefined, $scope.project.synchPromise());
