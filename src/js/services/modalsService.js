b7bc90137837f7860088516b91197f5b723ffb00
David Llop <llop-no-spam-please@protonmail.com>
2016-10-18 10:51:40 +0200

    Add missing file

---
+++ src/js/services/modalsService.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('Teem')
+  .factory('ModalsSvc', [
+  '$mdDialog',
+  function($mdDialog) {
+    // TODO: Populate with $mdDialog
+    return {
+      get: function() {},
+      set: function() {},
+      initialize: function() {},
+      turnOn: function() {},
+      turnOff: function() {}
+    };
+  }]);
