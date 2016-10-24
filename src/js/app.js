becd56f6d00503423d67370af93a15a93e4bebce
David Llop <llop-no-spam-please@protonmail.com>
2016-10-24 19:47:27 +0200

    Change theme colors

--- src/js/app.js
+++ src/js/app.js
@@ -73,10 +73,63 @@ angular
       .determinePreferredLanguage()
       .fallbackLanguage('en');
   })
-  .config(function($mdThemingProvider) {
+  .config(function ($mdThemingProvider) {
+    var customPrimary = {
+        '50': '#3fffe0',
+        '100': '#26ffdc',
+        '200': '#0cffd8',
+        '300': '#00f2cb',
+        '400': '#00d8b5',
+        '500': '#00bfa0',
+        '600': '#00a58b',
+        '700': '#008c75',
+        '800': '#007260',
+        '900': '#00594b',
+        'A100': '#59ffe4',
+        'A200': '#72ffe8',
+        'A400': '#8cffec',
+        'A700': '#003f35'
+    };
+    $mdThemingProvider
+        .definePalette('customPrimary',
+                        customPrimary);
+    $mdThemingProvider
+        .definePalette('customAccent',
+                        customPrimary);
+
+    var customWarn = {
+        '50': '#da98ce',
+        '100': '#d385c5',
+        '200': '#cc72bd',
+        '300': '#c65fb4',
+        '400': '#bf4dab',
+        '500': '#B2409E',
+        '600': '#9f398d',
+        '700': '#8c337d',
+        '800': '#7a2c6c',
+        '900': '#67255b',
+        'A100': '#e1aad7',
+        'A200': '#e7bde0',
+        'A400': '#eed0e9',
+        'A700': '#541e4b'
+    };
+    $mdThemingProvider
+        .definePalette('customWarn',
+                        customWarn);
     $mdThemingProvider.theme('default')
-      .primaryPalette('teal')
-      .accentPalette('purple');
+      .primaryPalette('customPrimary', {
+        'default': '400', // by default use shade 400 from the pink palette for primary intentions
+        'hue-1': '100', // use shade 100 for the `md-hue-1` class
+        'hue-2': '600', // use shade 600 for the `md-hue-2` class
+        'hue-3': 'A100' // use shade A100 for the `md-hue-3` class)
+      })
+      .accentPalette('customAccent', {
+        'default': '400', // by default use shade 400 from the pink palette for primary intentions
+        'hue-1': '100', // use shade 100 for the `md-hue-1` class
+        'hue-2': '600', // use shade 600 for the `md-hue-2` class
+        'hue-3': 'A100' // use shade A100 for the `md-hue-3` class)
+      });
+      //.warningPalette('customwarn');
   })
   .run(function(amMoment, $translate, $window, $rootScope) {
     amMoment.changeLocale($translate.proposedLanguage() || $translate.use());
