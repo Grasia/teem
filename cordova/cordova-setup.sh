5a55c4834c2c22bbea9eed283ade7c23557f2d32
Antonio Tenorio Fornés <antoniotenorio@ucm.es>
2016-11-17 13:30:31 +0100

    add cordova setup first script content

---
+++ cordova/cordova-setup.sh
@@ -0,0 +1,13 @@
+cordova platform add android
+
+# Whitelist plugin:
+cordova plugin add https://github.com/apache/cordova-plugin-whitelist.git
+
+#Phonegap push plugin:
+cordova plugin add phonegap-plugin-push
+
+#Camera plugin
+cordova plugin add cordova-plugin-camera
+
+#Crosswalk webview
+cordova plugin add cordova-plugin-crosswalk-webview
