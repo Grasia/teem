84a58db939410c8bdc7f17dfe0ae10eb129d2081
Antonio Tenorio Fornés <antoniotenorio@ucm.es>
2018-05-18 12:37:00 +0200

    push and run

--- .travis/script.sh
+++ .travis/script.sh
@@ -11,7 +11,7 @@ elif [ $TRAVIS_BRANCH = "master" ]; then
   eval "$(ssh-agent -s)" #start the ssh agent
   ssh-add .travis/id_rsa
 
-  if [[ $TRAVIS_COMMIT_MESSAGE == *"push and run"* ]]; then
+  if [[ $TRAVIS_COMMIT_MESSAGE == "push and run" ]]; then
       gulp cd:pushAndRun
   else
       gulp cd
